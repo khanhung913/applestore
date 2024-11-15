74d798cbc2d6860c1566316cd04733186dbe8806
Nguyen Khanh Hung <khanhung913@gmail.com>
2024-11-15 12:04:49 +0900

    hung

--- src/main/java/com/applestore/applestore/service/UploadService.java
+++ src/main/java/com/applestore/applestore/service/UploadService.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,7 +27,18 @@ public class UploadService {
             File dir = new File(rootPath + File.separator + targetFolder);
             if (!dir.exists())
                 dir.mkdirs();
-            finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+            int leftLimit = 97; // letter 'a'
+            int rightLimit = 122; // letter 'z'
+            int targetStringLength = 10;
+            Random random = new Random();
+            String generatedString = random.ints(leftLimit, rightLimit + 1)
+                    .limit(targetStringLength)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+            String defaultFileName = file.getOriginalFilename();
+            int a = defaultFileName.indexOf(".");
+            String subString = defaultFileName.substring(a, defaultFileName.length());
+            finalName = System.currentTimeMillis() + "-" + generatedString + subString;
             File serverFile = new File(dir.getAbsolutePath() + File.separator +
                     finalName);
             BufferedOutputStream stream = new BufferedOutputStream(
